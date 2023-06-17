bd5636f8dd06fa71e61384c5590d546f7c635c88
selcukatav <64098559+selcukatav@users.noreply.github.com>
2023-06-18 01:31:59 +0300

    Minesweeper score table adjusted and made the table store the datas

--- app/src/main/java/com/week2/minesweeper2/MainActivity.java
+++ app/src/main/java/com/week2/minesweeper2/MainActivity.java
@@ -2,29 +2,32 @@ package com.week2.minesweeper2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import com.week2.minesweeper2.views.grid.BaseCell;
-import com.week2.minesweeper2.views.grid.Cell;
-import com.week2.minesweeper2.views.grid.Grid;
-import com.week2.minesweeper2.GameEngine;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-    public int BOMB_NUMBER=4;
-
-    private int remainingMines;
-    Grid grid;
+    SharedPreferences sharedPreferences;
     TextView textView, bombCounter_view;
     Runnable runnable;
     Handler handler;
     int number;
+    int score;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gameplay);//MineSweeper Grid
 
         textView = findViewById(R.id.timerTxt);//timer text view
-
+        button = findViewById(R.id.saveScoreBtn);
         GameEngine.getInstance().createGrid(this);//creates grid on this activity.
 
         bombCounter_view = findViewById(R.id.bombCounter);
@@ -51,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         handler.post(runnable);
+        button.setEnabled(true);
     }
 
 
@@ -64,8 +68,44 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void saveScore(View view) {
 
 
+        String remainingTime = textView.getText().toString();
+        int number = Integer.parseInt(remainingTime);
+        int bombNumber = GameEngine.getInstance().BOMB_NUMBER;
+        int score = bombNumber * 400 / number;
+        int MAX_SCORE_NUMBER = 8;
+        //gets the score data and stores it in shared preferences.
+        sharedPreferences = getSharedPreferences("com.week2.minesweeper2", Context.MODE_PRIVATE);
+
+
+        Set<String> scoreSet = sharedPreferences.getStringSet("scores", new HashSet<String>());
+        ArrayList<Integer> scores = new ArrayList<>();
+        for (String scoreString : scoreSet) {
+            scores.add(Integer.parseInt(scoreString));
+        }
+        scores.add(score);
+
+        Collections.sort(scores, Collections.reverseOrder());
+
+        if (scores.size() > MAX_SCORE_NUMBER) {
+            scores = new ArrayList<>(scores.subList(0, MAX_SCORE_NUMBER));
+        }
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        scoreSet = new HashSet<>();
+        for (int scoreValue : scores) {
+            scoreSet.add(String.valueOf(scoreValue));
+        }
+        editor.putStringSet("scores", scoreSet);
+        editor.apply();
+        //sends data to ScoreTable.
+        Intent intent = new Intent(this, ScoreTable.class);
+        startActivity(intent);
+
+
+    }
 
 
 }
--- app/src/main/java/com/week2/minesweeper2/ScoreTable.java
+++ app/src/main/java/com/week2/minesweeper2/ScoreTable.java
@@ -2,19 +2,54 @@ package com.week2.minesweeper2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.content.SharedPreferences;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ScoreTable extends AppCompatActivity {
+    ListView listView;
 
+    private ArrayAdapter<Integer> adapter;
+
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_table);
+        listView = findViewById(R.id.scoreView);
+
+        sharedPreferences = getSharedPreferences("com.week2.minesweeper2", Context.MODE_PRIVATE);
+
+        //loads the scores on listView from sharedPreferences
+        Set<String> scoreSet = sharedPreferences.getStringSet("scores", new HashSet<String>());
+        ArrayList<Integer> scores = new ArrayList<>();
+        for (String scoreString : scoreSet) {
+            scores.add(Integer.parseInt(scoreString));
+        }
+
+        //lists scores reversed. new to old.
+        Collections.sort(scores, Collections.reverseOrder());
+
+        //creates listView adapter to send items to the listView
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, scores);
+        listView.setAdapter(adapter);
+
     }
     public void goBack(View view){
         Intent intent=new Intent(ScoreTable.this,MainMenu.class);
         startActivity(intent);
     }
+
+
 }
